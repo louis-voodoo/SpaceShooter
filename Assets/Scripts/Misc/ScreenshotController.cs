b052601c9c5fb38f62b7e068b20b8990f7d7c2de
Kevin WASER <kevin@voodoo.io>
2017-07-21 17:02:23 +0200

    Added ScreenshotFolder and Screenshots Appending

--- Assets/Scripts/Misc/ScreenshotController.cs
+++ Assets/Scripts/Misc/ScreenshotController.cs
@@ -1,20 +1,28 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 
 namespace EmptyGame.Misc {
 
 	public class ScreenshotController : MonoBehaviour {
-
 #if UNITY_EDITOR
+		public string ScreenshotFolder = "Screenshots";
 		private int count = 1;
+
+		void Start() {
+			if (!Directory.Exists(ScreenshotFolder)) {
+				Debug.Log("Created directory for screenshots: " + ScreenshotFolder);
+				Directory.CreateDirectory(ScreenshotFolder);
+			}
+			count = Directory.GetFiles(ScreenshotFolder).Length;
+		}
 
 		void Update() {
 			if (Input.GetMouseButtonDown(1)) {
-				string screenshotName = "Screenshot_" + count++ + ".png";
+				string screenshotName = ScreenshotFolder + "/Screenshot_" + count++ + ".png";
 				Application.CaptureScreenshot(screenshotName);
 				Debug.Log("Saved screenshot : \"" + screenshotName + "\"");
 			}
 		}
 #endif
 	}
-
 }
