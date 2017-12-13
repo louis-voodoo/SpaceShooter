fd483a6ad96ae258ad41c04338f1f8632bf53bc6
Louis Cabrera <louis@voodoo.io>
2017-12-13 16:26:52 +0100

    basic game scaffolding content

---
+++ Assets/Scripts/Basic/AnalyticsManager.cs
@@ -0,0 +1,14 @@
+﻿using GameAnalyticsSDK;
+
+namespace App {
+	public class AnalyticsManager {
+
+		public static void TrackGameStarted() {
+			GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, "game");
+		}
+
+		public static void TrackGameCompleted(int score) {
+			GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, "game", score);
+		}
+	}
+}
---
+++ Assets/Scripts/Basic/ForcePreload.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace EmptyGame.Misc {
+	public class ForcePreload : MonoBehaviour {
+		private void Awake() {
+			if (FindObjectOfType<Preload>() == null)
+				SceneManager.LoadScene(0, LoadSceneMode.Additive);
+		}
+	}
+}
