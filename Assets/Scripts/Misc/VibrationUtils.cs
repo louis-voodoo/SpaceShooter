e7e158d9be0be862ce4dc34a40999a33e38e4def
Maxime Philipp <maxime@studiopixmix.com>
2018-01-30 13:37:45 +0100

    Added the new haptic feedback VibrationUtils

---
+++ Assets/Scripts/Misc/VibrationUtils.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.iOS;
+
+namespace Misc {
+	public static class VibrationUtils {
+
+		private static readonly string VibrationEnabledKey = "VibrationUtils.VibrationEnabled";
+		
+		private static float lastVibration = 0;
+		private static float timeBetweenVibrations = 0.2f;
+		
+
+		public static void Vibrate() {
+			Vibrate(false);
+		}
+		
+		public static void Vibrate(bool ignoreVibrationCooldown) {
+			if (!VibrationEnabled())
+				return;
+			
+			if (ignoreVibrationCooldown || Time.unscaledTime - lastVibration > timeBetweenVibrations) {
+				lastVibration = Time.unscaledTime;
+				Debug.Log("Device generation : " + ((int) Device.generation));
+				if((int) Device.generation > 30)
+					HapticFeedback.DoHaptic(HapticFeedback.HapticForce.Medium);
+				else
+					Handheld.Vibrate();
+			}
+		}
+
+		public static bool VibrationEnabled() {
+			return PlayerPrefs.GetInt(VibrationEnabledKey, 1) == 1;
+		}
+
+		public static void ToggleVibrationEnabled() {
+			PlayerPrefs.SetInt(VibrationEnabledKey, (PlayerPrefs.GetInt(VibrationEnabledKey, 1) + 1) % 2);
+			
+			if(VibrationEnabled())
+				Vibrate(true);
+		}
+	}
+}
