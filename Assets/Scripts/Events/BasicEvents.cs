5fba248019f61e71d432e79d1fd0b5f1fcaacf29
Baptiste Dubessay <baptisted@voodoo.io>
2018-03-05 15:08:04 +0100

    Revert "Added game V1"
    This reverts commit 96a9ffc4c804c5573df35ce662cd9e6259f82dce.

--- Assets/Scripts/Events/BasicEvents.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class OnPlayerDeathEvent : GameEvent {}
-
-public class OnLaunchGameEvent : GameEvent {}
-
-public class OnResetGameEvent : GameEvent {}
